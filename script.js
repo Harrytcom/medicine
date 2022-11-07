003672e346809e3c844df766a54f1a4567a2721a
Harry Tkachenko <tharry@yandex.ru>
2022-11-07 16:09:05 +0300

    section header +toggle-menu

---
+++ script.js
@@ -0,0 +1,5 @@
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+	navBar.classList.toggle('toggle');
+});
